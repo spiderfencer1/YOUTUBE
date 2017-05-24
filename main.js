b732b903acbcd76196c6df0c10a1666cc7cd5998
spiderfencer1 <jinjoo@gmail.com>
2017-05-24 15:20:27 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
+function replace(str,key,value){
+	for(let i=0;i<str.length;i++){
+		if(str.substring(i,i+key.length) == key){
+			return str.substring(0,i) + value + str.substring(i+key.length,str.length);
+		}
+	}
+	return str;
+}
+
 window.onload = () => {
 	const buttons = document.getElementsByTagName('button');
 	for(let i=0;i<buttons.length;i++){
 		buttons[i].onclick = () => {
-			const text = document.getElementById("text" + (i + 1)).value;
-			let beginning = 0, end = 0;
-			for(let i=0;i<text.length;i++){
-				if(text.substring(i,i+4) == "src="){
-					i+=5;
-					beginning = i;
-					while(text[i]!='"'){
-						i++;
-					}
-					end = i;
-				}
-			}
-			console.log(text.substring(beginning,end));
-			document.getElementById("frame" + (i+1)).src = text.substring(beginning,end);
+			const text = replace(document.getElementById("text" + (i+1)).value,"watch?v=","embed/");
+			document.getElementById("frame" + (i+1)).src = text;
 		};
 	}
 };
